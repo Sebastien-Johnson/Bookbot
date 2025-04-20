b9f1c7dee147e15744f1a53fb9ce06f83cf7a9fa
Sebastien-Johnson <sebastien.z.johnson@gmail.com>
2025-04-20 00:05:33 -0400

    Refactored main to accept arguments, added more books to books

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
+import sys
 from stats import get_word_count, get_element_count, organize_info
+
 
 def get_book_text(f):
     return(f.read())
 
 
 def main():
-    with open("books/frankenstein.txt") as f:
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    with open(sys.argv[1]) as f:
         print("============ BOOKBOT ============")
-        print(f"Analyzing book found at {f.name}...")
+        print(f"Analyzing book found at {sys.argv[1]}...")
+
         w = get_book_text(f)
 
         get_word_count(w)
@@ -15,6 +22,7 @@ def main():
         d = get_element_count(w)
 
         organize_info(d)
+
         print("============= END ===============")
 
 main()
